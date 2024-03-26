682683e0106889194b49272c9118dae7c6ed9058
jnchiarot <natalina.chiarot@keyin.com>
2024-03-26 18:26:30 -0230

    Cleanup complete for routers and routes>api

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -1,28 +1,39 @@
+// Importing modules
 var router = require("express").Router();
 const moviesDal = require("../../services/movies.dal");
 
 // (GET) All movies
 router.get("/", async (req, res) => {
   try {
+    // Calling DAL function to get all movies
     let theMovies = await moviesDal.getMovies();
+    // Sending response with fetched movies in json format
     res.json(theMovies);
   } catch (error) {
+    // Logging error if it occured while fetching movies
     console.log("Error getting movies: ", error);
+    // Sending 500 error response
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
-// (GET) A movie by id
+// (GET) A movie by its id
 router.get("/:id", async (req, res) => {
   try {
+    // Calling DAL function to get a movie by its id
     let theMovie = await moviesDal.getMoviesById(req.params.id);
+    // Checking to see if the movie exists
     if (theMovie.length > 0) {
+      // If it exists, send it in response
       res.json(theMovie);
     } else {
+      // If it doesn't exist, send 404 response
       res.status(404).json({ message: "Not Found" });
     }
   } catch (error) {
+    // Logging error if it occured while fetching a movie by its id
     console.log("Error getting a movie by it's id: ", error);
+    // Sending 500 error response
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
@@ -30,15 +41,19 @@ router.get("/:id", async (req, res) => {
 // (POST) Add a new movie
 router.post("/", async (req, res) => {
   try {
+    // Call DAL function to add a new movie with the provided data
     await moviesDal.postMovie(
       req.body.title,
       req.body.release_date,
       req.body.director,
       req.body.description
     );
+    // Send 201 response
     res.status(201).json({ message: "Created Success" });
   } catch (error) {
+    // Logging error to console if one occured while adding a new movie
     console.log("Error adding new movie: ", error);
+    // Sending 500 error response
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
@@ -46,15 +61,19 @@ router.post("/", async (req, res) => {
 // (PUT) Replace an existing movie
 router.put("/:id", async (req, res) => {
   try {
+    // Call DAL function to replace an existing movie with the provided data
     await moviesDal.putMovie(
       req.params.id,
       req.body.title,
       req.body.director,
       req.body.description
     );
+    // Send 200 response
     res.status(200).json({ message: "OK Success" });
   } catch (error) {
+    // Logging error if any occured while replacing a movie
     console.log("Error replacing movie: ", error);
+    // Sending 500 error response
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
@@ -62,15 +81,19 @@ router.put("/:id", async (req, res) => {
 // (PATCH) Edit an existing movie
 router.patch("/:id", async (req, res) => {
   try {
+    // Call DAL function to edit an existing movie with the provided data
     await moviesDal.patchMovie(
       req.params.id,
       req.body.title,
       req.body.director,
       req.body.description
     );
+    // Send 200 response
     res.status(200).json({ message: "OK Success" });
   } catch (error) {
+    // Logging error if any occured while editing a movie
     console.log("Error: ", error);
+    // Sending 500 error response
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
@@ -78,10 +101,14 @@ router.patch("/:id", async (req, res) => {
 // (DELETE) A movie by its ids
 router.delete("/:id", async (req, res) => {
   try {
+    // Call DAL function to delete a movie by its id
     await moviesDal.deleteMovie(req.params.id);
+    // Send 200 response
     res.status(200).json({ message: "OK Success" });
   } catch (error) {
+    // Logging error if any occured while deleting a movie
     console.log("Error: ", error);
+    // Sending 500 error response
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -1,40 +1,58 @@
+// Importing modules
 var router = require("express").Router();
 const reviewsDal = require("../../services/reviews.dal");
 
+// Route to render review form for a movie
 router.get("/:movie_id/postreview", (req, res) => {
   try {
+    // Extracting the movie_id from request parameters
     const movie_id = req.params.movie_id;
+    // Sending movie_id as JSON response
     res.json(movie_id);
   } catch (error) {
+    // Handling any errors that occur
     console.log("Error getting review form: ", error);
+    // Sending 500 error response
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
+// Route to get all reviews for a movie
 router.get("/:movie_id/allreviews", async (req, res) => {
   try {
+    // Fetching all reviews for a movie by its movie_id
     let theReview = await reviewsDal.getReviewsById(req.params.movie_id);
+    // Checking if reviews exist
     if (theReview.length > 0) {
+      // If reviews exist, sending them as JSON response
       res.json(theReview);
     } else {
+      // If no reviews found, sending 404 response
       res.status(404).json({ message: "Not Found" });
     }
   } catch (error) {
+    // Handling any errors that occur
     console.log("Error getting a review by it's id: ", error);
+    // Sending 500 error response
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
+// Route to post a new review for a movie
 router.post("/:movie_id/postreview", async (req, res) => {
   try {
+    // Adding a new review for a movie with the provided data
     await reviewsDal.postReview(
       req.params.movie_id,
       req.body.rating,
       req.body.review_text
     );
+    // Sending a 201 response when successfully added
     res.status(201).json({ message: "Created Success" });
   } catch (error) {
+    // Handling any errors that occur
     console.log("Error adding new review: ", error);
+    // Sending 500 error response
     res.status(500).json({ message: "Internal Server Error" });
   }
 });
